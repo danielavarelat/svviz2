2310007e4fca99e743c49fffb18e066e98343a3c
Daniela Varela <varelad@jx19.cbio.private>
2019-05-09 14:24:17 -0400

    ..

--- src/svviz2/remap/maprealign.py
+++ src/svviz2/remap/maprealign.py
@@ -1,28 +1,30 @@
-
 def map_realign(batch, realigner, sample):
     # if sample.single_ended:
     #     return map_realign_unpaired(batch, realigner)
     # else:
     return map_realign_pairs(batch, realigner, sample)
 
+
 def map_realign_pairs(batch, datahub, sample):
     ref_genome_sources = [datahub.local_ref_genome_source]
     alt_genome_sources = [datahub.local_alt_genome_source]
 
-    if datahub.aligner_type == "bwa" and not datahub.args.only_realign_locally:
+    if datahub.aligner_type == "bwa":
         ref_genome_sources.append(datahub.genome)
 
-    for genome_source in ref_genome_sources+alt_genome_sources:
+    for genome_source in ref_genome_sources + alt_genome_sources:
         genome_source.set_aligner_params(sample.sequencer, sample.max_base_quality)
 
     import tqdm
+
     # for read_or_pair in batch:
     for read_or_pair in tqdm.tqdm(batch):
         # print(read_or_pair)
-        #if read_or_pair.name == "D00360:64:HBAP3ADXX:1:2114:9685:53802":
+        # if read_or_pair.name == "D00360:64:HBAP3ADXX:1:2114:9685:53802":
         read_or_pair.realign(ref_genome_sources, alt_genome_sources)
 
     return batch
+
 
 # def map_realign_pairs(batch, realigner, sample):
 #     results = []
